c02ac64aad6f5e8cb76340023560cecaf5dc3cf2
Tyler Oderkirk <t.y.l.e.r.oderkirk@gmail.com>
2015-06-25 21:41:35 -0400

    Nuked bogus shell script line.

--- make_slip_connection.sh
+++ make_slip_connection.sh
@@ -10,7 +10,6 @@ echo
 read -p 'Enter interface name (eg "sl0"): ' IFACE1
 sudo ifconfig ${IFACE1} 192.168.0.1 pointopoint 192.168.0.2 up
 sudo route add -host 192.168.0.2 dev ${IFACE1}
-2.168.0.1 dev ${IFACE2}
 
 echo "slattach pid: " ${SLATTACH_PID1} 
 echo "socat pid: " ${SOCAT_PID1} 
